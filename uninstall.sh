560e901e4924081c9c6703bf97c19829922cc634
AXIM0S <aminurfisa3@gmail.com>
2020-03-25 18:14:45 +0600

    Simplified the pre-coded uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,7 @@
+## For proper develoer documentation, visit https://supreme-gamers.com/gearlock
+
 # GEN_UNIS function is enabled for this prebuild-kernel package (Check `!zygote.sh`)
 # You don't need to modify this uninstall.sh
 
-if [ -e "$GRROOT/rescue.kernel" ]; then
-geco "\n+ Restoring old kernel zimage"
-rm "$GRROOT/kernel"
-mv "$GRROOT/rescue.kernel" "$GRROOT/kernel"
-chmod 777 "$GRROOT/kernel"
-fi
+R="$GRROOT/rescue.kernel"
+if [ -e "$R" ]; then geco "\n+ Restoring old kernel zimage ..."; sleep 1; rsync "$R" "$GRROOT/kernel"; fi
