b402faa23f3991dc9dd53bd884460a0d20651d5c
AXIM0S <aminurfisa3@gmail.com>
2020-04-08 23:14:27 +0600

    Some comment edits & simplification

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
-## Ready to use kernel script by @AXON
+## Ready to use kernel installation script by @AXON
 ## For proper develoer documentation, visit https://supreme-gamers.com/gearlock
-
-# Check `!zygote.sh` to configure your package functions or gearlock can also guide you during the build process
+# Check `!zygote.sh` to configure your package functions or gearlock can also guide you during the build process.
 
 #####--- Import Functions ---#####
-get_base_dir # Returns the directory path in $BD variable from where this *install.sh is being executed
+get_base_dir # Returns execution directory path in $BD variable
 #####--- Import Functions ---#####
 
 do_comm_job(){
@@ -16,12 +15,11 @@ do_comm_job(){
 		mv $GRROOT/kernel $GRROOT/rescue.kernel
 		geco "+ Your stock kernel zimage is renamed from kernel to rescue.kernel\n"
 	fi
-	nout gclone "$BD/kernel" "$GRROOT"
-	chmod 777 "$GRROOT/kernel"
-	sleep 1.5
+	rsync -a "$BD/kernel" "$GRROOT" && chmod 777 "$GRROOT/kernel" && sleep 1.5
 	geco "\n- Your system will reboot after 20 seconds. ${RED}Read the information below!${RC}"; sleep 1; source "$BD"/'!zygote.sh'
 	geco "\n- In case if your system is not booting with ${YELLOW}${NAME}-${VERSION}${RC} on your hardware,"
-	geco "\n- then you can rename ${PURPLE}rescue.kernel${RC} to ${GREEN}kernel${RC} on your android_x86 partition to boot with your old kernel..."; sleep 20
+	geco "\n- then you can rename ${PURPLE}rescue.kernel${RC} to ${GREEN}kernel${RC} on your android_x86 partition to boot with your old kernel ..."; sleep 20
+	# Cleanup package firmware if exists before gearlock gen_unins
 	[ -d "$BD/system/lib/firmware" ] && rm -r "$BD/system/lib/firmware"
 }
 
@@ -32,16 +30,13 @@ if [ -d "$BD/system/lib/firmware" ]; then
 
 		case $c in
 			[Yy]* )
-				geco "\n\n+ Deleting /system/firmware"
-				rm -r /system/lib/firmware
-				geco "\n+ Placing the kernel module and firmware files into your system"
-				do_comm_job
+				geco "\n\n+ Deleting /system/firmware" && rm -r /system/lib/firmware
+				geco "\n+ Placing the kernel module and firmware files into your system" && do_comm_job
 				break
 				;;
 
 			[Nn]* )
-				geco "\n+ Placing the kernel module files into your system"
-				do_comm_job
+				geco "\n+ Placing the kernel module files into your system" && do_comm_job
 				break
 				;;
 			* ) geco "\n- Enter either ${GREEN}y${RC}es or no" ;;
@@ -49,6 +44,5 @@ if [ -d "$BD/system/lib/firmware" ]; then
 	done
 
 else
-	geco "\n+ Placing the kernel module files into your system"
-	do_comm_job
+	geco "\n+ Placing the kernel module files into your system" && do_comm_job
 fi
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,5 @@
 ## For proper develoer documentation, visit https://supreme-gamers.com/gearlock
-
 # GEN_UNIS function is enabled for this prebuild-kernel package (Check `!zygote.sh`)
 # You don't need to modify this uninstall.sh
 
-R="$GRROOT/rescue.kernel"
-if [ -e "$R" ]; then geco "\n+ Restoring old kernel zimage ..."; sleep 1; rsync "$R" "$GRROOT/kernel"; fi
+R="$GRROOT/rescue.kernel"; if [ -f "$R" ]; then geco "\n+ Restoring old kernel zimage ..."; sleep 1; rsync "$R" "$GRROOT/kernel"; fi
