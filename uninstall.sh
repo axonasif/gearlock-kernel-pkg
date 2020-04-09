37592892a2ca63c3e18183444b01dda6638f8aab
AXIM0S <aminurfisa3@gmail.com>
2020-04-09 19:48:55 +0600

    Backup stock firmware

--- uninstall.sh
+++ uninstall.sh
@@ -2,4 +2,5 @@
 # GEN_UNIS function is enabled for this prebuild-kernel package (Check `!zygote.sh`)
 # You don't need to modify this uninstall.sh
 
-R="$GRROOT/rescue.kernel"; if [ -f "$R" ]; then geco "\n+ Restoring old kernel zimage ..."; sleep 1; rsync "$R" "$GRROOT/kernel"; fi
+R="$GRROOT/rescue.kernel"; if [ -f "$R" ]; then geco "\n+ Restoring stock kernel zimage ..."; sleep 1; rsync "$R" "$GRROOT/kernel"; fi
+[ -f $DEPDIR/firmware.bak ] && geco "\n+ Restoring stock firmware blobs ..." && nout garca x -aoa -o/system/lib $DEPDIR/firmware.bak && rm $DEPDIR/firmware.bak
