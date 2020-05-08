24856de7039aa4655b53ffb064090f6a10594566
AXIM0S <aminurfisa3@gmail.com>
2020-05-08 13:32:08 +0600

    Edit uninstall.sh for GearLock 6.1

--- uninstall.sh
+++ uninstall.sh
@@ -2,15 +2,14 @@
 # GEN_UNIS function is enabled for this prebuild-kernel package (Check `!zygote.sh`)
 # You don't need to modify this uninstall.sh
 
-R="$GRROOT/rescue.kernel"
-if [ -f "$R" ]; then 
-    geco "\n+ Restoring stock kernel zimage ..."; sleep 1
-    rsync "$R" "$GRROOT/kernel"
+if [ -f "${KERNEL_IMAGE}.rescue" ]; then 
+    geco "\n+ Restoring stock kernel image ..." && sleep 1
+    mv "${KERNEL_IMAGE}.rescue" "$KERNEL_IMAGE"
 fi
-if [ -f $DEPDIR/firmware.bak ]; then
+
+if [ -f "$DEPDIR/firmware.bak" ]; then
     geco "\n+ Restoring stock firmware blobs ..."
-    nout garca x -aoa -o/system/lib $DEPDIR/firmware.bak
-    rm $DEPDIR/firmware.bak
+    nout garca x -aoa -o/system/lib $DEPDIR/firmware.bak && rm $DEPDIR/firmware.bak
 fi
 
 # Clear dalvik-cache
