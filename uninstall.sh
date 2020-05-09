0076aa01e32f9fb95e31052223faf0a31c5fa20f
AXIM0S <aminurfisa3@gmail.com>
2020-05-09 22:03:04 +0600

    Add comments in uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,11 +2,13 @@
 # Auto uninstallation generation is enabled for this prebuild-kernel package (Check `!zygote.sh`)
 # You don't need to modify this uninstall.sh
 
+# Restore stock kernel image
 if [ -f "${KERNEL_IMAGE}.rescue" ]; then
     geco "\n+ Restoring stock kernel image ..." && sleep 1
     mv "${KERNEL_IMAGE}.rescue" "$KERNEL_IMAGE"
 fi
 
+# Restore backed up firmware blobs
 if [ -f "$DEPDIR/firmware.bak" ]; then
     geco "\n+ Restoring stock firmware blobs ..."
     nout garca x -aoa -o/system/lib "$DEPDIR/firmware.bak" && rm "$DEPDIR/firmware.bak"
