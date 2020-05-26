6fce12e6f6295d7db587c3b79e1f7a5883cb1b11
AXIM0S <aminurfisa3@gmail.com>
2020-05-27 00:42:44 +0600

    Error handling during uninstallation.

--- uninstall.sh
+++ uninstall.sh
@@ -10,14 +10,14 @@ DALVIKDIR="/data/dalvik-cache"
 # Restore stock kernel image
 if [ -f "$KERNEL_IMAGE.rescue" ]; then
 	geco "\n+ Restoring stock kernel image ..." && sleep 1
-	mv "$KERNEL_IMAGE.rescue" "$KERNEL_IMAGE"
+	mv "$KERNEL_IMAGE.rescue" "$KERNEL_IMAGE" || geco "\n++++ Error: Failed to restore stock kernel image" && return 101
 fi
 
 # Restore stock modules/firmware dir
 for tget in $KMODDIR $FIRMDIR; do
 	if [ -d $tget.old ]; then
 		geco "\n+ Restoring stock $(basename $tget) ..."
-		rm -rf $tget && mv $tget.old $tget
+		rm -rf $tget && mv $tget.old $tget || geco "\n++++ Error: Failed to restore stock $(basename $tget)" && return 101
 	fi
 done
 
